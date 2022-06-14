2bc882666fbf7db6e7f622d58a6a6eda1854707b
MohamedAlQadeery <moh9amad1@gmail.com>
2022-06-15 02:22:40 +0300

    HotelController simple crued /tmp data source DI

--- BookingApp/BookingApp.Api/Controllers/HotelsController.cs
+++ BookingApp/BookingApp.Api/Controllers/HotelsController.cs
@@ -1,17 +1,70 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookingApp.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookingApp.Api.Controllers
 {
     // url : /hotels
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class HotelsController : Controller
     {
-        [HttpGet]
-        public IActionResult GetRooms()
+        private readonly DataSource _data;
+
+        public HotelsController(DataSource dataSource)
         {
-            return Ok("Get Rooms Successfully");
+            _data = dataSource;
         }
 
+        [HttpGet]
+        public IActionResult GetHotels()
+        {
+            return Ok(_data.Hotels);
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateHotel([FromBody] Hotel hotel)
+        {
+            _data.Hotels.Add(hotel);
+            return CreatedAtAction(nameof(GetHotelById), new { id = hotel.Id }, hotel);
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        public IActionResult GetHotelById(int id)
+        {
+            var foundHotel = _data.Hotels.FirstOrDefault(hotel => hotel.Id == id);
+            if (foundHotel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(foundHotel);
+        }
+
+        [Route("{id}")]
+        [HttpPut]
+        public IActionResult UpdateHotel([FromBody] Hotel updatedHotel,int id)
+        {
+            var foundHotel = _data.Hotels.FirstOrDefault(h => h.Id == id);
+            if (foundHotel == null) return NotFound();
+            _data.Hotels.Remove(foundHotel);
+            _data.Hotels.Add(updatedHotel);
+
+            return NoContent();
+        }
+
+        [Route("{id}")]
+        [HttpDelete]
+        public IActionResult DeleteHotel(int id)
+        {
+            var foundHotel = _data.Hotels.FirstOrDefault(h => h.Id == id);
+            if (foundHotel == null) return NotFound();
+            _data.Hotels.Remove(foundHotel);
+
+            return NoContent();
+
+
+        }
     }
 }
---
+++ BookingApp/BookingApp.Api/DataSource.cs
@@ -0,0 +1,22 @@
+﻿using BookingApp.Domain.Models;
+
+namespace BookingApp.Api
+{
+    public class DataSource
+    {
+        public DataSource()
+        {
+            Hotels = GetHotels();
+        }
+
+       
+        public List<Hotel> Hotels { get; set; }
+
+        private List<Hotel>? GetHotels()
+        {
+            return new List<Hotel> { };
+        }
+
+
+    }
+}
